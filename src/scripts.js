8ac01b10353f13db4f1b1b360fa8bbe6aef141c5
mggsistema <mggsistema@gmail.com>
2024-05-18 17:01:15 -0300

    Primeiro commit-Projeto Previsão do Tempo

---
+++ src/scripts.js
@@ -0,0 +1,34 @@
+
+
+const key = "a9be1de665581f08f54feeb3862efd6b"
+             
+function colocarDadosNaTela(dados){
+    
+    document.querySelector(".cidade").innerHTML =  "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math .floor (dados.main.temp) + "°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+}
+
+async function buscarCidade(cidade) {
+    const dados = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&inits=metric`).then(resposta => resposta.json())
+
+    colocarDadosNaTela(dados)         
+
+}
+
+function cliqueiNoBotao() {
+    const cidade = document.querySelector(".input-cidade").value 
+    
+    buscarCidade(cidade)
+   
+}
+  
+ 
+  
+   
+
+
+
+   
